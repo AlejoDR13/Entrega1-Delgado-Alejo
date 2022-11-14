43aaaa6b791c4319156fc31abf52a2368cebde14
AlejoDR13 <alejodelgado199999@gmail.com>
2022-11-14 19:17:50 -0300

    Arreglo de urls
    Modificacion en la importacion de las views

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from AppCoder.views import padre, drama, terror, aventura, ciencia_ficcion, thriller, suspenso, sobremi, sesion, registro, crear_drama, buscar_drama, crear_terror, buscar_terror, crear_aventura, buscar_aventura, crear_cienciaficcion, buscar_cienciaficcion, crear_thriller, buscar_thriller, crear_suspenso, buscar_suspenso
 from AppCoder import views
 
 urlpatterns = [
@@ -15,26 +14,26 @@ urlpatterns = [
     path('registro/', views.registro, name="Registro"),
 
     #DRAMA
-    path('crear_drama/', crear_drama, name="AgregarDrama"),
+    path('crear_drama/', views.crear_drama, name="AgregarDrama"),
     path('buscar_drama/', views.buscar_drama),
 
     #TERROR
-    path('crear_terror/', crear_terror, name="AgregarTerror"),
+    path('crear_terror/', views.crear_terror, name="AgregarTerror"),
     path('buscar_terror/', views.buscar_terror),
 
     #AVENTURA
-    path('crear_aventura/', crear_aventura, name="AgregarAventura"),
+    path('crear_aventura/', views.crear_aventura, name="AgregarAventura"),
     path('buscar_aventura/', views.buscar_aventura),
 
     #CIENCIA FICCION
-    path('crear_cienciaficcion/', crear_cienciaficcion, name="AgregarCienciaFiccion"),
+    path('crear_cienciaficcion/', views.crear_cienciaficcion, name="AgregarCienciaFiccion"),
     path('buscar_cienciaficcion/', views.buscar_cienciaficcion),
     
     #THRILLER   
-    path('crear_thriller/', crear_thriller, name="AgregarThriller"),
+    path('crear_thriller/', views.crear_thriller, name="AgregarThriller"),
     path('buscar_thriller/', views.buscar_thriller),
 
     #SUSPENSO
-    path('crear_suspenso/', crear_suspenso, name="AgregarSuspenso"),
+    path('crear_suspenso/', views.crear_suspenso, name="AgregarSuspenso"),
     path('buscar_suspenso/', views.buscar_suspenso),
 ]
