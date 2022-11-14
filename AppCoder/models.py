8da465aa624cf122b8af6aad9aa63c50be150a04
AlejoDR13 <alejodelgado199999@gmail.com>
2022-11-14 20:42:20 -0300

    Correcciones Pablo

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -8,6 +8,8 @@ class Drama(models.Model):
     estreno = models.DateField()
     duracion = models.IntegerField()
     epigrafe = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return f"Titulo: {self.titulo}- direccion: {self.direccion} "
 
 class Terror(models.Model):
     titulo = models.CharField(max_length=40)
@@ -15,6 +17,8 @@ class Terror(models.Model):
     estreno = models.DateField()
     duracion = models.IntegerField()
     epigrafe = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return f"Titulo: {self.titulo}- direccion: {self.direccion} "
 
 class Aventura(models.Model):
     titulo = models.CharField(max_length=40)
@@ -22,27 +26,48 @@ class Aventura(models.Model):
     estreno = models.DateField()
     duracion = models.IntegerField()
     epigrafe = models.CharField(max_length=100)
-
+    def __str__(self) -> str:
+        return f"Titulo: {self.titulo}- direccion: {self.direccion} "
 class Ciencia_Ficcion(models.Model):
     titulo = models.CharField(max_length=40)
     direccion = models.CharField(max_length=40)
     estreno = models.DateField()
     duracion = models.IntegerField()
     epigrafe = models.CharField(max_length=100)
-
+    def __str__(self) -> str:
+        return f"Titulo: {self.titulo}- direccion: {self.direccion} "
 class Thriller(models.Model):
     titulo = models.CharField(max_length=40)
     direccion = models.CharField(max_length=40)
     estreno = models.DateField()
     duracion = models.IntegerField()
     epigrafe = models.CharField(max_length=100)
-
+    def __str__(self) -> str:
+        return f"Titulo: {self.titulo}- direccion: {self.direccion} "
 class Suspenso(models.Model):
     titulo = models.CharField(max_length=40)
     direccion = models.CharField(max_length=40)
     estreno = models.DateField()
     duracion = models.IntegerField()
     epigrafe = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return f"Titulo: {self.titulo}- direccion: {self.direccion} "
+
+
+#Consulta Pablo
+class Categorias(models.Model):
+    genero = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=40)
+
+class Peliculas(models.Model):
+    titulo = models.CharField(max_length=40)
+    direccion = models.CharField(max_length=40)
+    estreno = models.DateField()
+    duracion = models.IntegerField()
+    epigrafe = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+
+#Falta: Clase Posts, Clase AutorDelPost, Clase Categorias (Aca irian drama, terror, etc)
 
 #Falta todavia para esto
 
